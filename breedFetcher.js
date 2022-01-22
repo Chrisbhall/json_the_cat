42b4d3faa535145775011e642cb3951340689285
chris <chris.bhall@outlook.com>
2022-01-22 09:07:17 +0000

    refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,16 @@
 const request = require('request');
-const args = process.argv.slice(2);
-request("https://api.thecatapiaaaaaaaaaaa.com/v1/breeds/search?q=" + args[0], (error, response, body) => {
-  //console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  //console.log(typeof body);
-  if (body === "[]") {
-    console.log("Cat not found!!");
-  } else if (error) {
-    console.log('error:', error); // Print the error if one occurred
-  } else {
-    const data = JSON.parse(body);
-    console.log(data[0]["description"]);
-    //console.log(typeof data);
-  }
-});
+const fetchBreedDescription = function(breed, des) {
+  request("https://api.thecatapi.com/v1/breeds/search?q=" + breed, (error, response, body) => {
+    if (body === "[]") {
+      console.log("Cat not found!!");
+    } else if (error) {
+      console.log('error:', error); // Print the error if one occurred
+    } else {
+      const data = JSON.parse(body);
+      des(error,data[0]["description"]);
+      //console.log(data[0]["description"]);
+    }
+  });
+};
 
+module.exports = { fetchBreedDescription };
